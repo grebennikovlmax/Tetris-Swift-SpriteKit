5468adb99e4b23ea0ddd5b4021a2296615706cf3
GrebennikovMax <grebennikovlmax@gmail.com>
2020-05-22 00:16:05 +1000

    Initial Commit

---
+++ TetrisSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TetrisSwift
+//
+//  Created by Максим Гребенников on 22.05.2020.
+//  Copyright © 2020 Maksim Grebennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
